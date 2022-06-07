8ed5a02f6360fe6e305a8ab4904f7bb129b9258b
Zezzzzz <g.u.a.nze95@gmail.com>
2022-06-07 23:03:31 +0800

    added authentication, not done

--- Webservices/src/main/java/com/LuckBattle/Webservices/repository/RoleRepository.java
+++ Webservices/src/main/java/com/LuckBattle/Webservices/repository/RoleRepository.java
@@ -1,4 +1,13 @@
 package com.LuckBattle.Webservices.repository;
 
-public interface RoleRepository {
+import com.LuckBattle.Webservices.entity.ERole;
+import com.LuckBattle.Webservices.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(ERole name);
 }
